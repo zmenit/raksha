fb7289a9ce4b23f6b4a83e219ea7d8c4ab7e2237
Rakshitha <Rakshitha@Rakshitha>
2017-05-24 23:11:53 +0530

    autoconst

---
+++ src/generic/AutoConst.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface AutoConst {
+
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./driver/chromedriver.exe";
+	String GEKO_KEY="webdriver.gecko.driver";
+	String GEKO_VALUE="./driver/geckodriver.exe";
+	String SNAP_PATH="./snap";
+	String INPUT_PATH="./data/input.xlsx";
+	
+}
